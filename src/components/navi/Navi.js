be33a19871c43716c7bb04ce5be91793ffbbb17c
fatihguzel <20060393@stu.omu.edu.tr>
2022-11-07 17:10:27 +0300

    update reducer procces, change components

--- src/components/navi/Navi.js
+++ src/components/navi/Navi.js
@@ -30,7 +30,7 @@ export default class Navi extends React.Component {
     return (
       <div>
         <Navbar color="light" light expand="md">
-          <NavbarBrand href="/">reactstrap</NavbarBrand>
+          <NavbarBrand href="/">BugsySHP</NavbarBrand>
           <NavbarToggler onClick={this.toggle} />
           <Collapse isOpen={this.state.isOpen} navbar>
             <Nav className="ml-auto" navbar>
